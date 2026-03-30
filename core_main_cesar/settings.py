f089fa480f7e669fcd6b261c0ed4d795174ad06f
alvinsanchez93 <159292482+alvinsanchez93@users.noreply.github.com>
2026-03-30 09:31:58 -0700

    feat: implementacion de seguridad JWT y proteccion de rutas API

--- core_main_cesar/settings.py
+++ core_main_cesar/settings.py
@@ -123,7 +123,10 @@ STATIC_URL = 'static/'
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny"
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
